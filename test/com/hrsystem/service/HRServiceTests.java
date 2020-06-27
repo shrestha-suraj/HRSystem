8fe9201fb792c86370e60dd7451f13006850e8bd
Suraj Shrestha <shresthasuraj62@gmail.com>
2020-06-26 22:03:58 -0500

    Increase JUnit coverage testing by testing Exception cases as well

--- test/com/hrsystem/service/HRServiceTests.java
+++ test/com/hrsystem/service/HRServiceTests.java
@@ -1,6 +1,9 @@
 package com.hrsystem.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -48,4 +51,15 @@ public class HRServiceTests {
 		Employee searchedEmployee =hrService.searchEmployee(123, 1);
 		assertEquals(searchedEmployee.getFirstName(),employee.getFirstName());
 	}
+	
+	@Test
+	public void testCompanyNotFoundException() {
+		Exception exception = assertThrows(Exception.class,()->{
+			hrService.addCompany(new Company(123,"Test Company",null));
+		});
+		String expectedMessage="Company Id is taken. Try Again!\n";
+		String actualMessage=exception.getMessage();
+		assertTrue(actualMessage.contains(expectedMessage));
+	}
+	
 }
